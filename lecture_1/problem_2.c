1b151d0d8e4f54420fd55a224d80867f422b17cd
S. M. Redwan <smredwan.info@gmail.com>
2022-06-10 16:56:54 +0600

    commited after solve all problem of lecture_1

---
+++ lecture_1/problem_2.c
@@ -0,0 +1,9 @@
+//Printfing two sentances in different line by using \n
+
+#include <stdio.h>
+
+int main () {
+    printf("This first sentance.\nThis is secind sentance.\n");
+
+    return 0;
+}
---
+++ lecture_1/problem_3.c
@@ -0,0 +1,8 @@
+//printing a line with quatation
+
+#include <stdio.h>
+
+int main() {
+    printf("\"This line is print with quatation.\"\n");
+    return 0;
+}
