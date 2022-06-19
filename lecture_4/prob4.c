78d2dc0f6cea6e688070c3a7fc1029933657ec34
S. M. Redwan <smredwan.info@gmail.com>
2022-06-19 08:41:59 +0600

    Coding on problem 4

---
+++ lecture_4/prob4.c
@@ -0,0 +1,24 @@
+//leap year
+//Written by S. M. Redwan
+//ID: 221-35-828
+
+#include <stdio.h>
+
+int main() {
+    int inpt_year;
+
+    printf("Enter a year: ");
+    scanf("%d", &inpt_year);
+
+    if (inpt_year % 4 != 0) {
+        printf("%d is not leap year\n", inpt_year);
+    }
+    else if(inpt_year % 100 == 0 && inpt_year % 400 != 0) {
+        printf("%d is not leap year\n", inpt_year);
+    }
+    else {
+        printf("%d is leap year\n", inpt_year);
+    }
+
+    return 0;
+}
