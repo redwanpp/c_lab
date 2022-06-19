9b80421941520e9611c8a53e1b0c7f4f0f02e312
S. M. Redwan <smredwan.info@gmail.com>
2022-06-19 08:23:58 +0600

    Coding on problem 3

---
+++ lecture_4/prob3.c
@@ -0,0 +1,21 @@
+//Even odd
+//Written by S. M. Redwan
+//ID: 221-35-828
+
+#include <stdio.h>
+
+int main () {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num % 2 != 1) {
+        printf ("%d is even\n", num);
+    }
+    else {
+        printf("%d is odd\n");
+    }
+
+    return 0;
+}
