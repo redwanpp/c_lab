c4f300a828027adc71df67b25e757d5bc8c7b369
S. M. Redwan <smredwan.info@gmail.com>
2022-06-19 08:05:29 +0600

    Coding on lecture 4 problem 1

---
+++ lecture_4/prob1.c
@@ -0,0 +1,23 @@
+//finding maximu with if else
+//Written by S. M. Redwan
+//ID: 221-35-828
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    if(num1 > num2) {
+        printf("%d is maximum\n", num1);
+    }
+    else {
+        printf("%d is maximum\n", num2);
+    }
+
+    return 0;
+}
