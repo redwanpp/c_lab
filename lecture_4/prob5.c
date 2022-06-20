f6489ab3c0d7a52dedebd73480718f32d1971bd2
S. M. Redwan <smredwan.info@gmail.com>
2022-06-20 08:54:34 +0600

    Commited after coding on problem 5

---
+++ lecture_4/prob5.c
@@ -0,0 +1,21 @@
+//cheaking alphabet
+//written by S. M. Redwan
+//ID: 221-35-828
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+        printf("%c is an alphabet\n", ch);
+    }
+    else {
+        printf("%c is not an alphabet\n", ch);
+    }
+
+    return 0;
+}
