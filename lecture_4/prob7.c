b6a47ef46e74d73553e110bcd76b05815d6b3d8b
S. M. Redwan <smredwan.info@gmail.com>
2022-06-20 09:28:52 +0600

    Commited after coding on problem 7

---
+++ lecture_4/prob7.c
@@ -0,0 +1,28 @@
+//Is a trangle valid or not by angel
+//written by S. M. Redwan
+//ID: 221-35-828
+
+#include <stdio.h>
+
+int main() {
+    int first_angl, second_angl, third_angl, sum;
+
+    printf("Enter first angel here: ");
+    scanf("%d", &first_angl);
+    printf("Enter second angel here: ");
+    scanf("%d", &second_angl);
+    printf("Enter third angel here: ");
+    scanf("%d", &third_angl);
+
+    sum = first_angl + second_angl + third_angl;
+
+    if(sum == 180) {
+        printf("This triangle is valid\n");
+    }
+    else {
+        printf("This triangle is not valid\n");
+    }
+    
+
+    return 0;
+}
