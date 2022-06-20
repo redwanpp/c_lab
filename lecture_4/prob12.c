92c7881b2fb9444c2bc04bfbe9cd592dc48d4999
S. M. Redwan <smredwan.info@gmail.com>
2022-06-20 19:30:02 +0600

    Commited after coding on problem 12

---
+++ lecture_4/prob12.c
@@ -0,0 +1,31 @@
+//Electric bill
+//Written by S. M. Redwan
+//ID: 221-35-828
+
+#include <stdio.h>
+
+int main() {
+    float unit, bill;
+
+    printf("Enter Unit: ");
+    scanf("%f", &unit);
+
+    if (unit < 201) {
+        bill = unit * 0.5;
+        printf("Your bill is %.2f\n", bill);
+    }
+    else if(unit < 401) {
+        bill = 100 + ((unit - 200) * 0.65);
+        printf("Your bill is %.2f\n", bill);
+    }
+    else if(unit < 601) {
+        bill = 230 + ((unit - 400) * 0.8);
+        printf("Your bill is %.2f\n", bill);
+    }
+    else {
+        bill = 390 + ((unit - 600) * 1.0);
+        printf("Your bill is %.2f\n", bill);
+    }
+
+    return 0;
+}
