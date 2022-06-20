a523eabd557056c797a1f566431768f5e8f40f5c
S. M. Redwan <smredwan.info@gmail.com>
2022-06-20 23:09:35 +0600

    problem 4 complete

---
+++ lecture_6/prob4.c
@@ -0,0 +1,31 @@
+//Finding minimum from an array
+//Written by S. M. Redwan
+//ID: 221-35-828
+
+#include <stdio.h>
+
+int main() {
+    int n, i, min;
+
+    printf("Enter array size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements below: \n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    min = 0;
+
+    for(i = 0; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("The minimum value of the array is: %d\n", min);
+
+    return 0;
+}
